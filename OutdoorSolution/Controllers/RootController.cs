181465500bea01996ada61ff8657831db6a2dea1
Ostap Kravchuk <ostap1010@gmail.com>
2015-12-13 21:15:52 +0100

    Create User link to account actions and user data in the root response

--- OutdoorSolution/Controllers/RootController.cs
+++ OutdoorSolution/Controllers/RootController.cs
@@ -10,12 +10,21 @@ namespace OutdoorSolution.Controllers
 {
     public class RootController : ApiController
     {
+        private const string USER_NODES_ROUTE = "api/User";
+
         public IHttpActionResult Get()
         {
             var nodes = new Dictionary<string, object>();
-            nodes.Add("Version", Assembly.GetExecutingAssembly().GetName().Version.ToString());
+            nodes.Add( "Version", Assembly.GetExecutingAssembly().GetName().Version.ToString() );
             nodes.Add( "PreviewAreas", Url.Link<PreviewAreasController>(c => c.Get(null)) );
+            nodes.Add( "User", Url.GetSpecialResource(USER_NODES_ROUTE) );
 
+            return Ok(nodes);
+        }
+
+        [Route(USER_NODES_ROUTE)]
+        public IHttpActionResult GetUserLink()
+        {
             // add account data
             var accountNodes = new Dictionary<string, object>();
             
@@ -23,8 +32,7 @@ namespace OutdoorSolution.Controllers
             if (User.Identity.IsAuthenticated)
             {
                 var userId = new Guid(User.Identity.GetUserId());
-                nodes.Add( "UserInfo", Url.Link<UserInfoController>(c => c.GetById(userId)) );
-                
+                accountNodes.Add("UserInfo", Url.Link<UserInfoController>(c => c.GetById(userId)));
                 accountNodes.Add("ChangePassword", Url.GetSpecialResource("/api/Account/ChangePassword"));
                 //accountNodes.Add( "Logout", Url.Link<AccountController>(c => c.Logout()) );
                 accountNodes.Add("Logout", Url.GetSpecialResource("/api/Account/Logout"));
@@ -36,9 +44,7 @@ namespace OutdoorSolution.Controllers
                 accountNodes.Add("Register", Url.GetSpecialResource("/api/Account/Register", HttpMethods.Post));
             }
 
-            nodes.Add( "Account", accountNodes );
-
-            return Ok(nodes);
+            return Ok(accountNodes);
         }
     }
 }
